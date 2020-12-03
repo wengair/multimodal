c46fef23889104983284cac187bc36b87293a56a
Chia-Tse, Weng <51400167+wengair@users.noreply.github.com>
2020-12-02 16:05:01 -0800

    Update DataContext.js

--- frontend/src/components/DataContext.js
+++ frontend/src/components/DataContext.js
@@ -3,14 +3,14 @@ import React, {createContext, useState, useEffect} from 'react'
 export const Data = createContext(null)
 
 function DataContext({children}) {
-  const [instances, setInstances] = useState()
+  const [predictions, setPredictions] = useState()
 
   const readDataJson = () => {
     fetch('/data/200_sample_val_conv.json')
       .then(res => res.json())
       .then(result => {
         console.log(result)
-        setInstances(result)
+        setPredictions(result)
       })
       .catch(e => console.log(e))
   }
